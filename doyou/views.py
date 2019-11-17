e2960325053715bdead846c1e4858edfdf475a04
Haemanth S P <haemanthsp@gmail.com>
2019-11-17 18:30:48 +0100

    Create mock APIs

--- doyou/views.py
+++ doyou/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+from rest_framework import status
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
+import base64
+
+
+class GetWordList(APIView):
+        def post(self, req):
+            data = {"wordList": ['Quickly', 'Happy', 'new', 'winter']}
+            return Response(data=data, status=status.HTTP_200_OK)
+
+
+class PostActivity(APIView):
+    def post(self, req):
+        username = json.loads(req.body)['username']
+        data = {'username': username}
+        return Response(data=data, status=status.HTTP_200_OK)
