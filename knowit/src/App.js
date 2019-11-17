01d72e2ff4d7e824cb3f9c6362a60fa28195ac50
Haemanth S P <haemanthsp@gmail.com>
2019-11-17 18:31:24 +0100

    Add functionality to UI

--- knowit/src/App.js
+++ knowit/src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -7,22 +8,38 @@ class App extends Component {
      constructor(props) {
         super(props);
         this.state = {
-            wordlist: [],
-            word: "Word",
+            user: "Me",
+            wordList: [],
+            activeWordIndex: 0,
+            selection: "",
+            selections: [],
         }
    }
+
+
+  getWordList = (event) => {
+    let stateData = this.state;
+    this.setState({ isLoading: true })
+    const user = {
+      username: stateData.user,
+    };
+    let config = { "Content-Type": "application/json" };
+    axios.post('http://localhost:8000/api/v1/getwordlist', user, config)
+      .then(response => {this.setState({wordList: response.data.wordList})})
+  }
 
   render() {
     return (
         <div className= "container">
+        <button onClick={() => {this.getWordList()}} > load </button>
         <div id="progressbar" style={{marginTop: "50px"}}>
-          <div style={{ width: "100%" }}></div>
+          <div style={{ width: (this.state.wordList.length - this.state.activeWordIndex) * 5 + "%" }}></div>
         </div>
-        <h1 className="word"> Word </h1>
+        <h1 className="word"> {this.state.wordList[this.state.activeWordIndex]} </h1>
         <div className="col-md-12">
           <div className="row card_ctr">
-            <button className="button green" onClick={() => {this.setState({activePage: "words"})}}> Yes </button>
-            <button className="button red" onClick={() => {this.setState({activePage: "words"})}}> No </button>
+            <button className="button green" onClick={() => {this.setState({selection: "yes"})}}> Yes </button>
+            <button className="button red" onClick={() => {this.setState({selection: "no"})}}> No </button>
           </div>
         </div>
         </div>
