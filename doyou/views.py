4a3baee4c3fb3378d379dfaca62fb63e3755b5f0
Haemanth S P <haemanthsp@gmail.com>
2019-11-22 18:32:47 +0100

    Create Api to collect testcases for all levels

--- doyou/views.py
+++ doyou/views.py
@@ -8,15 +8,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView
 import base64
+import pickle
 
 from doyou import tokens
 
 
 class GetWordList(APIView):
-        def post(self, req):
-            data = {"wordList": tokens.get_wordlist("./data/hp.txt")}
-            # data = {"wordList": ['Quickly', 'Happy', 'new', 'winter']}
-            return Response(data=data, status=status.HTTP_200_OK)
+    def post(self, req):
+        # data = {"wordList": tokens.get_wordlist("./data/hp.txt")}
+        data = {"wordList": ['Quickly', 'Happy', 'new', 'winter']}
+        return Response(data=data, status=status.HTTP_200_OK)
+
+
+class GetLevels(APIView):
+    def post(self, req):
+        vocab_test = pickle.load(open("./data/vocab_tests/PaulMeera.p", 'rb'))
+        data = {"levels": vocab_test}
+        # data = {"levels": ['A1', 'A2']}
+        return Response(data=data, status=status.HTTP_200_OK)
 
 
 class PostActivity(APIView):
