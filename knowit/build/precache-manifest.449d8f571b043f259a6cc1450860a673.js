bc2bf3c95cece929a878e144ae45576d5647cadc
Haemanth S P <haemanthsp@gmail.com>
2020-01-31 17:20:46 +0100

    Add the built app to repo

---
+++ knowit/build/precache-manifest.449d8f571b043f259a6cc1450860a673.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11ab538de2864ce2744e5a2978bcd24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5806a9f2a2ef40d51afc",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "cf5cb9c0504405f263da",
+    "url": "/static/css/main.d40644d2.chunk.css"
+  },
+  {
+    "revision": "5806a9f2a2ef40d51afc",
+    "url": "/static/js/2.79025801.chunk.js"
+  },
+  {
+    "revision": "cf5cb9c0504405f263da",
+    "url": "/static/js/main.8edc72f2.chunk.js"
+  },
+  {
+    "revision": "ce077ccea755c9ffe6f1",
+    "url": "/static/js/runtime-main.980aa254.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
