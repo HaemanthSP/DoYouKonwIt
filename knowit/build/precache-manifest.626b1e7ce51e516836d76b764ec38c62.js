da30ac68cd8441fce72b21adecc1d1d3573e1c0b
Haemanth S P <haemanthsp@gmail.com>
2020-02-04 16:07:30 +0100

    Update the binary for new machine

---
+++ knowit/build/precache-manifest.626b1e7ce51e516836d76b764ec38c62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89311141674f03e303f54b04db2bd699",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5806a9f2a2ef40d51afc",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "de2b1fbadf96d9de33bd",
+    "url": "/static/css/main.d40644d2.chunk.css"
+  },
+  {
+    "revision": "5806a9f2a2ef40d51afc",
+    "url": "/static/js/2.79025801.chunk.js"
+  },
+  {
+    "revision": "de2b1fbadf96d9de33bd",
+    "url": "/static/js/main.8612f94f.chunk.js"
+  },
+  {
+    "revision": "ce077ccea755c9ffe6f1",
+    "url": "/static/js/runtime-main.980aa254.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
