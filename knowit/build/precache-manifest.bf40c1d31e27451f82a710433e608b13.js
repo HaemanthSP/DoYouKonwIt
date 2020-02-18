3ccc7b91daf36181222563f5da74ce5391e462e3
Haemanth S P <haemanthsp@gmail.com>
2020-02-18 18:13:11 +0100

    Adds build for remote

---
+++ knowit/build/precache-manifest.bf40c1d31e27451f82a710433e608b13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600e61c02b3af070e290e8064f9739b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1045fcf81b9ac2126266",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "d2eedfd8640c80e8aa62",
+    "url": "/static/css/main.bdd0c042.chunk.css"
+  },
+  {
+    "revision": "1045fcf81b9ac2126266",
+    "url": "/static/js/2.eaeb15d4.chunk.js"
+  },
+  {
+    "revision": "d2eedfd8640c80e8aa62",
+    "url": "/static/js/main.7e6886c5.chunk.js"
+  },
+  {
+    "revision": "ce077ccea755c9ffe6f1",
+    "url": "/static/js/runtime-main.980aa254.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
