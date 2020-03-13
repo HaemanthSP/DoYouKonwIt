c964f014a8d19fc2c10e45c2bacdf3569d518a0c
Haemanth S P <haemanthsp@gmail.com>
2020-03-13 16:54:45 +0100

    Updated app

---
+++ knowit/build/precache-manifest.b65381969d658e6a1b315f20ba9ba1d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5faad88442c92e1f42216a8510d2db1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ba5a44d8642ce00b864",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "6b42067796e07e58e4f5",
+    "url": "/static/css/main.b7dd62de.chunk.css"
+  },
+  {
+    "revision": "4ba5a44d8642ce00b864",
+    "url": "/static/js/2.63b6c41a.chunk.js"
+  },
+  {
+    "revision": "6b42067796e07e58e4f5",
+    "url": "/static/js/main.0f107d58.chunk.js"
+  },
+  {
+    "revision": "ce077ccea755c9ffe6f1",
+    "url": "/static/js/runtime-main.980aa254.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
