63a852fbb984e58326cd7830b7985d3e40feb77b
Haemanth S P <haemanthsp@gmail.com>
2020-05-07 15:20:46 +0200

    Update app in remote

---
+++ knowit/build/precache-manifest.9b099b6c847d3e3d22b245934b81af4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3afe18e52d440f043df47127e7a14341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ba5a44d8642ce00b864",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "3ee1fa30d2f383c3e2ba",
+    "url": "/static/css/main.d22de9c5.chunk.css"
+  },
+  {
+    "revision": "4ba5a44d8642ce00b864",
+    "url": "/static/js/2.63b6c41a.chunk.js"
+  },
+  {
+    "revision": "3ee1fa30d2f383c3e2ba",
+    "url": "/static/js/main.627252f7.chunk.js"
+  },
+  {
+    "revision": "ce077ccea755c9ffe6f1",
+    "url": "/static/js/runtime-main.980aa254.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
